bad81c56dfb07958a92cbb398c9c4af3f32984c8
linjin <linjin@sixnology.com>
2014-11-26 18:25:42 +0800

    recyclerview version. need to clean up though.

--- TaiwanTidal/app/src/main/java/com/tada/taiwantidal/MainActivity.java
+++ TaiwanTidal/app/src/main/java/com/tada/taiwantidal/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -43,6 +44,9 @@ public class MainActivity extends ActionBarActivity {
     String townName;
     TidalIdChart tidalIdChart;
 
+    MyAdapter townAdapter;
+    RecyclerView townsRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,6 +70,61 @@ public class MainActivity extends ActionBarActivity {
             Log.i("TaiwanTidal", "tidalIdChart.getAllPalces error:" + e.toString());
         }
 
+        RecyclerView citiesRecyclerView = (RecyclerView) findViewById(R.id.citiesList);
+        citiesRecyclerView.setHasFixedSize(true);
+        citiesRecyclerView.addItemDecoration(new DividerItemDecoration(MainActivity.this, DividerItemDecoration.VERTICAL_LIST));
+        // use a linear layout manager
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        citiesRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        MyAdapter citiesAdapter = new MyAdapter(cities);
+        citiesRecyclerView.setAdapter(citiesAdapter);
+
+
+        citiesRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getApplicationContext(), new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        try {
+                            JSONArray townsArray = tidalIdChart.getTowns(position);
+                            towns = new String[townsArray.length()];
+                            tidalIds = new String[townsArray.length()];
+                            for(int i = 0; i < townsArray.length(); i ++) {
+                                JSONObject place = townsArray.getJSONObject(i);
+                                String townName = place.getString("townName");
+                                tidalIds[i] = place.getString("id");
+                                towns[i] = getString(getResources().getIdentifier(townName, "string", getPackageName()));
+                                townAdapter.resetData(towns);
+                                townAdapter.notifyDataSetChanged();
+                            }
+                        } catch (Exception e) {
+                            Log.i("TaiwanTidal", "tidalIdChart.getTowns error:" + e.toString());
+                        }
+                    }
+                })
+        );
+
+
+        towns = new String[0];
+        // town recyclerView
+        townsRecyclerView = (RecyclerView) findViewById(R.id.townsList);
+        townsRecyclerView.setHasFixedSize(true);
+        townsRecyclerView.addItemDecoration(new DividerItemDecoration(MainActivity.this, DividerItemDecoration.VERTICAL_LIST));
+        LinearLayoutManager townsLayoutManager = new LinearLayoutManager(this);
+        townsRecyclerView.setLayoutManager(townsLayoutManager);
+
+        // specify an adapter (see also next example)
+        townAdapter = new MyAdapter(towns);
+        townsRecyclerView.setAdapter(townAdapter);
+        townsRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getApplicationContext(), new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        getTidalInfo(tidalIds[position]);
+                    }
+                })
+        );
+
+        /*
         citiesListView = (ListView)findViewById(R.id.citiesList);
         ArrayAdapter<String> citiesAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, cities);
@@ -108,7 +167,7 @@ public class MainActivity extends ActionBarActivity {
                 });
             }
         });
-
+        */
     }
 
     public void getTidalInfo(String tidalId){
@@ -141,4 +200,84 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }*/
 
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        private String[] mDataset;
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public TextView mTextView;
+            public ViewHolder(View v) {
+                super(v);
+                mTextView = (TextView)v.findViewById(R.id.textView);
+            }
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public MyAdapter(String[] myDataset) {
+            mDataset = myDataset;
+        }
+
+        public void resetData(String[] myDataset){
+            mDataset = myDataset;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+            // create a new view
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.row_listview, parent, false);
+            // set the view's size, margins, paddings and layout parameters
+            ViewHolder vh = new MyAdapter.ViewHolder(v);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            holder.mTextView.setText(mDataset[position]);
+
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
+    }
+
+    public static class RecyclerItemClickListener implements RecyclerView.OnItemTouchListener {
+        private OnItemClickListener mListener;
+
+        public interface OnItemClickListener {
+            public void onItemClick(View view, int position);
+        }
+
+        GestureDetector mGestureDetector;
+
+        public RecyclerItemClickListener(Context context, OnItemClickListener listener) {
+            mListener = listener;
+            mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+            });
+        }
+
+        @Override public boolean onInterceptTouchEvent(RecyclerView view, MotionEvent e) {
+            View childView = view.findChildViewUnder(e.getX(), e.getY());
+            if (childView != null && mListener != null && mGestureDetector.onTouchEvent(e)) {
+                mListener.onItemClick(childView, view.getChildPosition(childView));
+            }
+            return false;
+        }
+
+        @Override public void onTouchEvent(RecyclerView view, MotionEvent motionEvent) { }
+    }
 }
